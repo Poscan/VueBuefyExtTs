379bcc0df71fa219afe7f6b4cf1938e71b06ce2d
Александров Даниил <alexandrov_da@medinfocenter.ru>
2021-12-09 18:42:48 +0500

    add stimulsoft report

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app">
+    <div>
+      <h2>Stimulsoft Reports.JS Designer</h2>
+      <div id="designer"></div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  mounted: function () {
+    var options = new window.Stimulsoft.Designer.StiDesignerOptions();
+    options.appearance.fullScreenMode = false;
+
+    var designer = new window.Stimulsoft.Designer.StiDesigner(
+      options,
+      "StiDesigner",
+      false
+    );
+
+    var report = new window.Stimulsoft.Report.StiReport();
+
+    report.loadFile("/reports/SimpleList.mrt");
+
+    designer.report = report;
+
+    designer.renderHtml("designer");
+  },
+};
+</script>
+
+<style>
+</style>
