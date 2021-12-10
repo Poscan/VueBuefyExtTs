652708c97edd6916f17feb664c52ebcd1332e590
Александров Даниил <alexandrov_da@medinfocenter.ru>
2021-12-10 12:58:07 +0500

    Add component StimulsoftDesign

---
+++ src/components/StimulsoftDesigner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Stimulsoft Reports.JS Designer</h2>
+    <div id="designer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+
+  props: ["report"],
+
+  data() {
+    return {};
+  },
+
+  mounted: function () {
+    var options = new window.Stimulsoft.Designer.StiDesignerOptions();
+    options.appearance.fullScreenMode = false;
+
+    var designer = new window.Stimulsoft.Designer.StiDesigner(
+      options,
+      "StiDesigner",
+      false
+    );
+
+    designer.report = this.report;
+
+    designer.renderHtml("designer");
+  },
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,38 +1,29 @@
 <template>
   <div id="app">
-    <div>
-      <h2>Stimulsoft Reports.JS Designer</h2>
-      <div id="designer"></div>
-    </div>
+    <StimulsoftDesigner :report="report" />
   </div>
 </template>
 
 <script>
-export default {
+import Vue from "vue";
+import StimulsoftDesigner from "@/components/StimulsoftDesigner.vue";
+
+export default Vue.extend({
+  components: {
+    StimulsoftDesigner,
+  },
+
   name: "app",
   data() {
     return {};
   },
-  mounted: function () {
-    var options = new window.Stimulsoft.Designer.StiDesignerOptions();
-    options.appearance.fullScreenMode = false;
 
-    var designer = new window.Stimulsoft.Designer.StiDesigner(
-      options,
-      "StiDesigner",
-      false
-    );
-
-    var report = new window.Stimulsoft.Report.StiReport();
-
-    report.loadFile("/reports/SimpleList.mrt");
-
-    designer.report = report;
-
-    designer.renderHtml("designer");
+  computed: {
+    report() {
+      var report = new window.Stimulsoft.Report.StiReport();
+      report.loadFile("/reports/SimpleList.mrt");
+      return report;
+    },
   },
-};
+});
 </script>
-
-<style>
-</style>
